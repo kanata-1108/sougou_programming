49fbd658618ac49cbb45ebbc01f10b878900ba95
kanata-1108 <kanatako08@yahoo.co.jp>
2021-06-13 02:01:00 +0900

    20210613

---
+++ 3-2/kadai-2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void){
+    FILE *fp;
+    char str[4][10];//[文字列数][文字列の長さのmax]
+    int data[10][4];
+    int i, num, japanese, math, english;
+    int j, t, k, l, n = 3;
+
+    fp = fopen("kadai-2.csv", "r");
+
+    if(fp == NULL){
+        printf("failed\n");
+        return 1;
+    }
+
+    //fscanfでcsvの最初の1行を読み込む
+    //%[^,]は「,」が現れるまで読み込む,%sは空白文字が現れるまで読み込む
+    fscanf(fp, "%[^,],%[^,],%[^,],%s", str[0], str[1], str[2], str[3]);
+    printf("%s %s %s %s\n", str[0], str[1], str[2],str[3]);
+
+    for(i = 0; i < 3;i++){
+        fscanf(fp, "%d,%d,%d,%d", &data[i][0], &data[i][1], &data[i][2], &data[i][3]);
+        printf("%d %d %d %d\n", data[i][0], data[i][1], data[i][2], data[i][3]);
+    }
+    fclose(fp);
+
+    for(l = 0; l < n - 1; l++){
+        for(j = n - 1; j > l; j--){
+            if(data[j-1][1] < data[j][1]){
+                for(k = 0; k < 4; k++){
+                    t = data[j][k];
+                    data[j][k] = data[j-1][k];
+                    data[j-1][k] = t;
+                }
+            }
+        }
+   }
+   for(i = 0; i < 3;i++){
+       printf("%d %d %d %d\n", data[i][0], data[i][1], data[i][2], data[i][3]);
+    }
+
+    return 0;
+}
+
