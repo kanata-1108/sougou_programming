ab8deba5b9413a32e0bf8f6157c9a7c5f68f58f8
kanata-1108 <kanatako08@yahoo.co.jp>
2021-06-13 16:07:16 +0900

    20210613

--- 3-6/kadai-6-2.c
+++ 3-6/kadai-6-2.c
@@ -5,7 +5,7 @@ int main(void){
     FILE *after_fp;
     int i, j, k, l;
     char str[17][100];
-    float seiseki[15][17];
+    int seiseki[15][17];
     char *num = "受験番号";
 
     fp = fopen("kadai-6.csv", "r");
@@ -20,9 +20,9 @@ int main(void){
     fprintf(after_fp, "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", num, str[0], str[1], str[2], str[3], str[4], str[5], str[6], str[7], str[8], str[9], str[10], str[11]);
 
     for(i = 1; i < 16; i++){
-        fscanf(fp, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f", 
+        fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", 
         &seiseki[i][0], &seiseki[i][1], &seiseki[i][2], &seiseki[i][3], &seiseki[i][4], &seiseki[i][5], &seiseki[i][6], &seiseki[i][7], &seiseki[i][8], &seiseki[i][9], &seiseki[i][10], &seiseki[i][11]);
-        fprintf(after_fp, "%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+        fprintf(after_fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
         i, seiseki[i][0], seiseki[i][1], seiseki[i][2], seiseki[i][3], seiseki[i][4], seiseki[i][5], seiseki[i][6], seiseki[i][7], seiseki[i][8], seiseki[i][9], seiseki[i][10], seiseki[i][11]);
     }
 
--- 3-6/kadai-6-4.c
+++ 3-6/kadai-6-4.c
@@ -3,14 +3,17 @@
 int main(void){
     FILE *fp;
     FILE *after_fp;
-    int i, j, k, l;
+    int i, j, k, l, m;
     char str[17][100];
     int seiseki[15][17];
     char *soten = "素点";
     char *kiroku = "記録点";
     char *num = "受験番号";
+    char *kanzan_kiroku = "換算記録点";
     int soten_value;
     int kiroku_value;
+    int kiroku_kanzan_value;
+    float kiroku_kanzan = 700.0 / 49.0;
 
     fp = fopen("kadai-6.csv", "r");
     after_fp = fopen("after_kadai-6.csv", "w");
@@ -21,7 +24,7 @@ int main(void){
     }
 
     fscanf(fp, "%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%s", str[0], str[1], str[2], str[3], str[4], str[5], str[6], str[7], str[8], str[9], str[10], str[11]);
-    fprintf(after_fp, "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", num, str[0], str[1], str[2], str[3], str[4], str[5], str[6], str[7], str[8], str[9], str[10], str[11], soten, kiroku);
+    fprintf(after_fp, "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", num, str[0], str[1], str[2], str[3], str[4], str[5], str[6], str[7], str[8], str[9], str[10], str[11], soten, kiroku, kanzan_kiroku);
 
     for(i = 1; i < 16; i++){
         fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d", 
@@ -32,11 +35,23 @@ int main(void){
         for(l = 0; l < 9; l++){
             kiroku_value += seiseki[i][l + 3];
         }
-        fprintf(after_fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
-        i, seiseki[i][0], seiseki[i][1], seiseki[i][2], seiseki[i][3], seiseki[i][4], seiseki[i][5], seiseki[i][6], seiseki[i][7], seiseki[i][8], seiseki[i][9], seiseki[i][10], seiseki[i][11], soten_value, kiroku_value);
+        for(m = 3; m < 12; m++){
+            if(m == 5 || m == 6){
+                kiroku_kanzan_value += seiseki[i][m] * 1.4;
+                printf("%d\n", kiroku_kanzan_value);
+            }else{
+                kiroku_kanzan_value += seiseki[i][m];
+                printf("%d\n", kiroku_kanzan_value);
+            }
+        }
+        kiroku_kanzan_value = kiroku_kanzan_value * kiroku_kanzan;
+
+        fprintf(after_fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
+        i, seiseki[i][0], seiseki[i][1], seiseki[i][2], seiseki[i][3], seiseki[i][4], seiseki[i][5], seiseki[i][6], seiseki[i][7], seiseki[i][8], seiseki[i][9], seiseki[i][10], seiseki[i][11], soten_value, kiroku_value, kiroku_kanzan_value);
 
         soten_value = 0;
         kiroku_value = 0;
+        kiroku_kanzan_value = 0;
     }
 
     return 0;
